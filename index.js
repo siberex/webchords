198315ff1c70f11e931aaf33f25844efb670899c
Stepan Legachev <siberex@gmail.com>
2017-03-22 19:47:28 +0300

    added entrypoint stub

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by sib.li on 22.03.17.
+ */
+
+"use strict";
+
+(function (undefined) {
+    //
+
+}());
+
+(async function () {
+    //
+
+})().catch(err => {
+    console.error(err);
+    //process.exit();
+});
+
+class index {
+    constructor(...args) {
+        this.args = args;
+    }
+} // index class
+
+// module.exports = index;
